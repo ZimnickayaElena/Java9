7917eaf16d3badf6fc05c03cfe663327c376bf9e
Elena Zimnickaya <nazarova.leno4ka@yandex.ru>
2023-04-07 19:39:16 +0400

    Initial Commit

---
+++ src/main/java/Post.java
@@ -0,0 +1,10 @@
+public class Post {
+    public String name; // поле для имени
+    public String surname; // поле для фамилии
+    public String patronimic; // поле для  отчества
+    public String passport; // поле для паспорта
+    public String phone; // поле для номера телефона
+    public boolean subscription; // поле для смс-оповещения
+
+    public FormDate birthday; // поле для даты рождения
+}
